9b908282c219faef61c8e361d720080875427126
vudinhquang <vudinhquangk53@gmail.com>
2020-07-05 00:28:59 +0900

    Content

--- p04-changeText/js/content.js
+++ p04-changeText/js/content.js
@@ -1,5 +1,7 @@
 chrome.runtime.onMessage.addListener(getMessage);
 
 function getMessage( message,  sender,  sendResponse) {
-    console.log(message);
+    if(message.task == "changeText"){
+        $('h1,h2,h3,h4,h5,h6,span,a,p').html(message.val);
+    }
 }
