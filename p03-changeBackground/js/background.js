0935d8e74c13eb3eb9e09dca6b1c123bd8ef4893
vudinhquang <vudinhquangk53@gmail.com>
2020-07-04 22:44:20 +0900

    Message, GetMesssage

--- p03-changeBackground/js/background.js
+++ p03-changeBackground/js/background.js
@@ -1,6 +1,14 @@
 chrome.browserAction.onClicked.addListener(iconExtensionClicked);
 
+function random_item(items)
+{
+    return items[Math.floor(Math.random()*items.length)];
+}
+
 function iconExtensionClicked(tab){
-    console.log(123);
-    console.log(tab);
+    let color = random_item(["#FF5733", "#3CFF33", "#E5F985", "#1D45F7", "#F71DF0"]);
+    chrome.tabs.sendMessage(tab.id, {
+        task: "changeBackgroundColor", // changeTextColor
+        valueColor: color
+    });
 }
--- p03-changeBackground/js/content.js
+++ p03-changeBackground/js/content.js
@@ -1,3 +1,11 @@
-$(document ).ready(function() {
-    console.log("hehe");
-});
+chrome.runtime.onMessage.addListener(getMessage);
+
+function getMessage( message,  sender,  sendResponse) {
+    if(message.task == "changeBackgroundColor"){
+        $("body").css("background-color", message.valueColor);
+    }
+
+    if(message.task == "changeTextColor"){
+        $("a").css("color", message.valueColor);
+    }
+}
