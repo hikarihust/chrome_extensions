728e9bc38e72cb3c335e317a5f9ae164e18aa227
vudinhquang <vudinhquangk53@gmail.com>
2020-07-05 04:41:40 +0900

    Optimize

---
+++ p05-api/js/content_backup.js
@@ -0,0 +1,12 @@
+console.log("contentjs");
+
+window.addEventListener('mouseup', function(){
+    let selectedText = window.getSelection().toString().trim();
+    selectedText     = selectedText.replace(/\s{2,}/g, '');
+    if(selectedText.length > 0) {
+        chrome.runtime.sendMessage({
+            task: "findCourse",
+            courseStr: selectedText
+        });
+    }
+});
