efe315b412b6ed5c4927134ad398e9f661ff1705
vudinhquang <vudinhquangk53@gmail.com>
2020-07-06 01:18:03 +0900

    Validate

--- p07-tasklist/js/popup.js
+++ p07-tasklist/js/popup.js
@@ -1,1 +1,19 @@
-console.log("popup.js");
+$( document ).ready(function() {
+    
+    let $elmListTask         = $('div#area-list-task');
+    let $inputTaskName       = $('input[name="input-task-name"]');
+    let $btnAddTask          = $('button[name="btn-add-task"]');
+
+    $inputTaskName.keyup(function(e) {
+        if ($inputTaskName.val().length >= 3) {
+            $btnAddTask.attr('disabled', false);
+            if (e.keyCode === 13) $btnAddTask.trigger('click');
+        } else {
+            $btnAddTask.attr('disabled', true);
+        }
+    });
+    
+    $btnAddTask.click(()=> {
+        console.log("btnAddTask");
+    })
+});
