cf006d5198ecbd4eb12c3cee15e74c5fe9a1e38f
vudinhquang <vudinhquangk53@gmail.com>
2020-07-06 00:48:42 +0900

    Load config

--- p07-tasklist/js/options.js
+++ p07-tasklist/js/options.js
@@ -1,3 +1,11 @@
-chrome.storage.sync.get(['config'], function(result) {
-    console.log(result);
+$(document).ready(function () {
+    let $btnSave                    = $('button[name="btn-save"]');
+    let $inputExtensionBackground   = $('input[name="extension_background"]');
+    let $inputListTaskShow          = $('select[name="list_task_show"]');
+
+    chrome.storage.sync.get('config', function(result) {
+        let config = result.config;
+        $inputExtensionBackground.val(config.extension_background);
+        $inputListTaskShow.val(config.list_task_show);
+    });
 });
