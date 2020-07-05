0329f57aae3ebf2a3032f312a6a8834d31a50963
vudinhquang <vudinhquangk53@gmail.com>
2020-07-06 00:59:50 +0900

    Save config, restore

--- p07-tasklist/js/options.js
+++ p07-tasklist/js/options.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     let $btnSave                    = $('button[name="btn-save"]');
+    let $btnRestore                 = $('button[name="btn-restore-default"]');
     let $inputExtensionBackground   = $('input[name="extension_background"]');
     let $inputListTaskShow          = $('select[name="list_task_show"]');
 
@@ -7,5 +8,31 @@ $(document).ready(function () {
         let config = result.config;
         $inputExtensionBackground.val(config.extension_background);
         $inputListTaskShow.val(config.list_task_show);
+    });
+
+    // Lưu cấu hình cho extension
+    $btnSave.click(() => {
+        let updateConfig = {
+            'extension_background': $inputExtensionBackground.val(),
+            'list_task_show'      : $inputListTaskShow.val()
+        }
+        chrome.storage.sync.set({
+            config: updateConfig
+        }, function() {
+            close();
+        });
+    });
+
+    // Restore cấu hình cho extension
+    $btnRestore.click(() => {
+        let initConfig = {
+            'extension_background': '#F71DF0',
+            'list_task_show'      : 'all'
+        }
+        chrome.storage.sync.set({
+            config: initConfig
+        }, function() {
+            close();
+        });
     });
 });
