22ec5d79fbd634ad4e68fa3a122e63952b46387b
vudinhquang <vudinhquangk53@gmail.com>
2020-07-05 17:40:04 +0900

    Highlight

--- p06-highlight/js/content.js
+++ p06-highlight/js/content.js
@@ -3,5 +3,18 @@ chrome.runtime.onMessage.addListener(getMessage);
 function getMessage( message,  sender,  sendResponse) {
     if(message.task == "hightlight"){
         console.log(message.selectionText);
+
+        let htmlReplace = `<span style="
+                        color:red; 
+                        font-size:20px;
+                        background-color:yellow;
+                        font-weight:bold;
+                    ">${message.selectionText}</span>`;
+        // php
+        $('p:contains("' + message.selectionText + '")').each(function() {
+            let text = $(this).text();
+            text     = text.replace(new RegExp(message.selectionText, 'g'), htmlReplace);
+            $(this).html(text);
+        });
     }
 }
