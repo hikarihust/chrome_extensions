563f06a217365fc42d4c008d0a7b7d8fe983925f
vudinhquang <vudinhquangk53@gmail.com>
2020-07-09 00:49:04 +0900

    Hiển thị list task

--- p07-tasklist/js/popup.js
+++ p07-tasklist/js/popup.js
@@ -4,6 +4,8 @@ $( document ).ready(function() {
     let $inputTaskName       = $('input[name="input-task-name"]');
     let $btnAddTask          = $('button[name="btn-add-task"]');
 
+    getAllTasks($elmListTask, 'all');
+
     $inputTaskName.keyup(function(e) {
         if ($inputTaskName.val().length >= 3) {
             $btnAddTask.attr('disabled', false);
@@ -31,3 +33,36 @@ $( document ).ready(function() {
         });
     })
 });
+
+function getAllTasks($elmListTask, status = null) {
+    chrome.storage.sync.get('tasks', (data) => {
+        let currentListTask = (data.hasOwnProperty('tasks')) ? data.tasks : [];
+        let total           = currentListTask.length;
+        let xhtmlResult     = "";
+
+        if(total > 0) {
+            for(let i = 0; i < total; i++) {
+                let currentItem     = currentListTask[i];
+                let textLineThrough = '';
+                let buttonCompleted = `<button class="status btn-finish mr-1" data-index="${ i }">Hoàn thành</button>`;
+    
+                if(currentItem.status === 'completed'){
+                    textLineThrough = 'text-line-through';
+                    buttonCompleted = '';
+                }
+    
+                xhtmlResult += `<div class="task-item mb-2">
+                    <div class="w-50 title-${ i } ${textLineThrough}"><span class="task-index">${ i + 1}</span>${currentItem.name}</div>
+                    <div class="task-item__button">
+                        ${buttonCompleted}
+                        <button class="delete btn-delete" data-index="${ i }">Xóa</button>
+                    </div>
+                </div>`;
+            }
+        } else {
+            xhtmlResult = "Chưa có công việc nào";
+        }
+
+        $elmListTask.html(xhtmlResult);
+    });
+}
